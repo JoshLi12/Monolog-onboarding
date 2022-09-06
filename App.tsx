2b6e5b4800a946f2a839631e37ff7652590ceeba
Josh Li <j.li20061212@gmail.com>
2022-09-05 20:52:18 -0400

    testing commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Pressable, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import { Picker } from '@react-native-picker/picker';
+
 
 export default function App() {
+  const [selectedLanguage, setSelectedLanguage] = React.useState();
+
+  const handleSignUp = () => {
+    // TODO: validate user's data
+
+    /*
+    if (success) {
+      Alert.alert(
+        "User Signup", `Language: ${selectedLanguage}`
+      );
+      // navigate to next page
+    } else {
+      // Alert to display error message
+    }
+    */
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Hello" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+
+      <Pressable
+        onPress={handleSignUp}
+        style={styles.signUpButton}
+      >
+        <Text style={styles.signUpText}>
+          Sign Up
+        </Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -13,8 +49,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  signUpButton: {
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'green'
+  },
+  signUpText: {
+    color: "red",
+    fontSize: 10
+  }
 });
